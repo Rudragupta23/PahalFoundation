27413de349102023b7c59fc57f0271624a6e0192
Rudragupta23 <23rudragupta@gmail.com>
2026-08-24 01:20:29 +0530

    Chnaged aws creditanls

--- PahalFoundation/PahalFoundation/settings.py
+++ PahalFoundation/PahalFoundation/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = f'https://pahalbucket-v2.s3.amazonaws.com/'
+MEDIA_URL = f'https://pahalbucket-v3.s3.amazonaws.com/'
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
